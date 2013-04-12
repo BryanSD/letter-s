2443302a2c1d93b0913cf90f084e13102a000b16
Bryan Davidson <bryan.davidson@rackspace.com>
2013-04-12 13:46:35 -0400

    Queue Stats tracker swallows exceptions
    Enables stats to continue running if it receives an error every
    once and a while

--- letter-s/queue_stats.py
+++ letter-s/queue_stats.py
@@ -24,18 +24,21 @@ if __name__ == "__main__":
     s.connect((sys.argv[2], int(sys.argv[3])))
 
     while True:
-        task_message_stats = queue_tasks.get_stats().messages
-        tasks = task_message_stats['total']
-        tasks_expired = task_message_stats['expired']
-        graphite_message = 'openstack.queue.work.count %d %d\n' % (
-            tasks - tasks_expired, int(time.time()))
-        s.sendall(graphite_message)
+        try:
+            task_message_stats = queue_tasks.get_stats().messages
+            tasks = task_message_stats['total']
+            tasks_expired = task_message_stats['expired']
+            graphite_message = 'openstack.queue.work.count %d %d\n' % (
+                tasks - tasks_expired, int(time.time()))
+            s.sendall(graphite_message)
 
-        response_message_stats = queue_responses.get_stats().messages
-        responses = response_message_stats['total']
-        responses_expired = response_message_stats['expired']
-        graphite_message = 'openstack.queue.result.count %d %d\n' % (
-            responses - responses_expired, int(time.time()))
-        s.sendall(graphite_message)
+            response_message_stats = queue_responses.get_stats().messages
+            responses = response_message_stats['total']
+            responses_expired = response_message_stats['expired']
+            graphite_message = 'openstack.queue.result.count %d %d\n' % (
+                responses - responses_expired, int(time.time()))
+            s.sendall(graphite_message)
+        except Exception as ex:
+            print ex
 
     s.close()
